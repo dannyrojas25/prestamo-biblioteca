c918bbf77439c3e6a8f8e6b498f160eb52a5616e
Administrador <ingalexisrojas2@gmail.com>
2024-04-26 08:03:13 -0500

    Clases eliminadas

--- src/main/java/com/ceiba/biblioteca/seguridad/ConstantesSeguridad.java
+++
@@ -1,6 +0,0 @@
-package com.ceiba.biblioteca.seguridad;
-
-public class ConstantesSeguridad {
-    public static final long JWT_EXPIRATION_TOKEN = 300000;
-    public static final String JWT_FIRMA = "firma";
-}
